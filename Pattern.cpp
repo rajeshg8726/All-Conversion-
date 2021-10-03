5f702de9b45ba95dac04aec1b4f7a23ce3322b9d
RAJESH GUPTA <rajeshg8726@gmail.com>
2021-10-03 20:42:25 +0530

    Basic Patterns Problems

---
+++ Pattern.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // for rectangle pattern;
+
+    // int row, col;
+    // cin>> row >> col;
+    // for (int  i = 1; i <= row; i++)
+    // {
+    //     for (int j = 1; j <= col; j++)
+    //     {
+    //         cout << " " << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for hollow rectangle .
+    // i is use as Rows and j is use as Columns.
+
+    // for (int  i = 1; i <= row; i++)
+    // {
+    //     for (int j = 1; j <= col; j++)
+    //     {
+    //         if (i==1 || i==row || j==1 || j==col)
+    //         {
+    //             cout << "*" ;
+    //         }
+    //         else
+    //         {
+    //             cout << " ";
+    //         }
+
+    //     }
+    //     cout << endl;
+    // }
+
+    //  FOR INVERTED HALF Pyramid
+
+    // int n;
+    // cin >> n;
+    // for (int i = n; i >= 1; i--)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << " * ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for inverted half pyramid after 180 degree rotation .
+
+    // int n;
+    // cin >> n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n-i; j++)
+    //     {
+    //         cout <<"  ";
+    //     }
+    //     for (int j = 1; j <= i; j++)
+    //    {
+    //         cout <<" *";
+    //     }
+
+    //     cout << endl;
+    // }
+
+    // for inverted half triangle by using numbers.
+
+    // int n;
+    // cin >> n;
+    // for (int i = n; i >= 1; i--)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << " " << j;
+    //     }
+
+    //     cout << endl;
+    // }
+
+    // int n;
+    // cin >> n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << " " << i;
+    //     }
+
+    //     cout << endl;
+    // }
+
+    // Floyd's Triangle.
+
+    // int n;
+    // int count = 1;
+    // cin >> n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << " " << count;
+    //         count++;
+    //     }
+
+    //     cout << endl;
+    // }
+
+    //  for Zero One Pattern .
+
+    // int n;
+    // cin >> n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         if ((i + j) % 2 == 0)
+    //         {
+    //             cout << "1";
+    //         }
+    //         else
+    //         {
+    //             cout << "0";
+    //         }
+    //     }
+
+    //     cout << endl;
+    // }
+
+    return 0;
+}
